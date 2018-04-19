44507841faef53e8a0f35ff7b86ec6edd85e76d7
Kamil <kamil@kamil.com>
2018-04-19 16:31:20 +0200

    First Version

---
+++ main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    // Menu-toggle button
+    $(document).ready(function () {
+        $(".menu-icon").on("click", function () {
+            $("nav ul").toggleClass("showing");
+        });
+    });
+
+    // Scrolling Effect
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop()) {
+            $('nav').addClass('black');
+        } else {
+            $('nav').removeClass('black');
+        }
+    });
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+
+    var $button = $('#menu-btn');
+
+    $('.menu-icon').on('click', function (e) {
+        e.preventDefault();
+        if ($button.hasClass('open')) {
+            $button.removeClass('open');
+            $button.addClass('close');
+        } else {
+            $button.removeClass('close');
+            $button.addClass('open');
+        }
+    });
+
+});
